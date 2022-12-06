3a88469e4406451eb674555e9b2a27630a993754
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 16:18:42 +0530

    Initial Commit

---
+++ DiffForm/DiffFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiffFormApp.swift
+//  DiffForm
+//
+//  Created by Bhumika Patel on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DiffFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
