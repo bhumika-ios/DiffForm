b5496da56bc2f35c74a103db29388762c7206edb
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-09 13:46:07 +0530

    date picker used for date formate - hidden for used labelhidden()

--- DiffForm/SecondFormwithValidation/DateFormView.swift
+++ DiffForm/SecondFormwithValidation/DateFormView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct DateFormView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            DatePicker("", selection: .constant(Date()), displayedComponents: [.date])
+               .labelsHidden()
+        }
     }
 }
 
