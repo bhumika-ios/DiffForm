f483c0535688386e30176175ea9b1ea5526fd0d8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-09 13:28:33 +0530

    create new group next form with validation

---
+++ DiffForm/SecondFormwithValidation/TextFieldFormView.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldFormView.swift
+//  DiffForm
+//
+//  Created by Bhumika Patel on 09/12/22.
+//
+
+import SwiftUI
+
+struct TextFieldFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFieldFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldFormView()
+    }
+}
