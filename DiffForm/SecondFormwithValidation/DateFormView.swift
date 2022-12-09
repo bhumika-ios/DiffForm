48bc71bd0d99c7f7ee77f3d436c720b7ec7e7704
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-09 13:43:09 +0530

    new file create dateformview

---
+++ DiffForm/SecondFormwithValidation/DateFormView.swift
@@ -0,0 +1,20 @@
+//
+//  DateFormView.swift
+//  DiffForm
+//
+//  Created by Bhumika Patel on 09/12/22.
+//
+
+import SwiftUI
+
+struct DateFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateFormView()
+    }
+}
