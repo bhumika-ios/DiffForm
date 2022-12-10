dc37b16dca4598f2152cac2c7fcd83b3e9be50f4
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-10 15:04:43 +0530

    date validation add in date picker

--- DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
+++ DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
@@ -17,7 +17,7 @@ final class FormContentBuilderImpl:ObservableObject, FormContentBuilder {
         TextFormComponent(id: .firstName, placeholder: "First Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid first name entered"))])]),
         TextFormComponent(id: .lastName, placeholder: "Last Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid last name entered"))])]),
         TextFormComponent(id: .email, placeholder: "Email", keyboardType: .emailAddress, validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.emailChars, error: .custom(message: "Invalid Email missing @")), RegexFormItem(pattern: RegexPatterns.higherThanSixChars, error: .custom(message: "Less than 6 characters"))])]),
-        DateFormComponent(id: .dob, mode: .date),
+        DateFormComponent(id: .dob, mode: .date, validations: [DateValidationManagerImpl()]),
         ButtonFormComponent(id: .submit, title: "Confirm")
     ]
 }
