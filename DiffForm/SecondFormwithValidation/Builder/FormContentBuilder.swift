19674f78382f5afa5a57b3d398edd711131c9e7c
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-10 15:00:35 +0530

    validation call on textfield

--- DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
+++ DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
@@ -14,8 +14,8 @@ protocol FormContentBuilder {
 
 final class FormContentBuilderImpl:ObservableObject, FormContentBuilder {
     private(set) var content:[FormComponent] = [
-        TextFormComponent(id: .firstName, placeholder: "First Name"),
-        TextFormComponent(id: .lastName, placeholder: "Last Name"),
+        TextFormComponent(id: .firstName, placeholder: "First Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid first name entered"))])]),
+        TextFormComponent(id: .lastName, placeholder: "Last Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid last name entered"))])]),
         TextFormComponent(id: .email, placeholder: "Email", keyboardType: .emailAddress),
         DateFormComponent(id: .dob, mode: .date),
         ButtonFormComponent(id: .submit, title: "Confirm")
