8c8db8505937ccb116f28f7eaa056b2745c0b7b1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-10 15:03:39 +0530

    email validation add on email textfield

--- DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
+++ DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
@@ -16,7 +16,7 @@ final class FormContentBuilderImpl:ObservableObject, FormContentBuilder {
     private(set) var content:[FormComponent] = [
         TextFormComponent(id: .firstName, placeholder: "First Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid first name entered"))])]),
         TextFormComponent(id: .lastName, placeholder: "Last Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid last name entered"))])]),
-        TextFormComponent(id: .email, placeholder: "Email", keyboardType: .emailAddress),
+        TextFormComponent(id: .email, placeholder: "Email", keyboardType: .emailAddress, validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.emailChars, error: .custom(message: "Invalid Email missing @")), RegexFormItem(pattern: RegexPatterns.higherThanSixChars, error: .custom(message: "Less than 6 characters"))])]),
         DateFormComponent(id: .dob, mode: .date),
         ButtonFormComponent(id: .submit, title: "Confirm")
     ]
