779b2891c760e7655fc9227c3aeb66249f6089d9
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-12 12:27:59 +0530

    func validate create for submit button

--- DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
+++ DiffForm/SecondFormwithValidation/Builder/FormContentBuilder.swift
@@ -12,9 +12,13 @@ protocol FormContentBuilder {
     var content: [FormComponent] { get }
     var state: FormState? { get }
     func update(_ val: Any, in component: FormComponent)
+    func validate()
 }
 
 final class FormContentBuilderImpl:ObservableObject, FormContentBuilder {
+
+    @Published private(set) var state: FormState?
+    
     private(set) var content:[FormComponent] = [
         TextFormComponent(id: .firstName, placeholder: "First Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid first name entered"))])]),
         TextFormComponent(id: .lastName, placeholder: "Last Name", validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid last name entered"))])]),
@@ -25,5 +29,27 @@ final class FormContentBuilderImpl:ObservableObject, FormContentBuilder {
     func update(_ val: Any, in component: FormComponent){
         guard let index = content.firstIndex(where: {$0.id == component.id}) else { return }
         content[index].val = val
+    }
+    func validate(){
+        let formComponents = content
+            .filter{ $0.formId != .submit }
+        
+        for component in formComponents {
+            for validator in component.validations{
+                if let error = validator.validate(component.val as Any){
+                    self.state = .fail(error: error)
+                    return
+                }
+            }
+        }
+        if let firstName = formComponents.first(where: { $0.formId == .firstName })?.val as? String,
+           let lastName = formComponents.first(where: { $0.formId == .lastName })?.val as? String,
+           let email = formComponents.first(where: { $0.formId == .email })?.val as? String,
+           let dob = formComponents.first(where: { $0.formId == .dob })?.val as? Date {
+            let newUser = User(firstName: firstName, lastName: lastName, email: email, dob: dob)
+            
+            self.state = .valid(user: newUser)
+            
+        }
     }
 }
