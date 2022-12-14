abf7d7f84aa0b533c2e5db7bb9ffb3e9774c2967
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-14 13:51:25 +0530

    creat entry field different way used for calling view

--- DiffForm/ThirdView/ThirdContentView.swift
+++ DiffForm/ThirdView/ThirdContentView.swift
@@ -14,7 +14,9 @@ struct ThirdContentView: View {
         ZStack{
             VStack{
                 VStack{
-                    
+                    EntryField(sfSymbolName: "envelope", placeHolder: "Email Address", prompt: <#String#>, field: $viewM.email)
+                    EntryField(sfSymbolName: "lock", placeHolder: "Password", prompt: <#String#>,  field: $viewM.password, isSecure: true)
+                    EntryField(sfSymbolName: "lock", placeHolder: "Confirm", prompt: <#String#>,  field: $viewM.confirmPw, isSecure: true)
                 }
             }
         }
@@ -24,5 +26,33 @@ struct ThirdContentView: View {
 struct ThirdContentView_Previews: PreviewProvider {
     static var previews: some View {
         ThirdContentView()
+    }
+}
+struct EntryField: View {
+    var sfSymbolName: String
+    var placeHolder: String
+    var prompt: String
+    @Binding var field: String
+    var isSecure:Bool = false
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: sfSymbolName)
+                    .foregroundColor(.gray)
+                    .font(.headline)
+                if isSecure {
+                    SecureField(placeHolder, text: $field).autocapitalization(.none)
+                } else {
+                    
+                    TextField(placeHolder, text: $field).autocapitalization(.none)
+                }
+            }
+            .padding(8)
+            .background(Color(UIColor.secondarySystemBackground))
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+            Text(prompt)
+            .fixedSize(horizontal: false, vertical: true)
+            .font(.caption)
+        }
     }
 }
