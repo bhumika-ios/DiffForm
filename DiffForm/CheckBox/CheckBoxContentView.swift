e8180958c23aed85024a3ffcc46798aa7f35d5a9
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-15 11:02:00 +0530

    show task name with box

--- DiffForm/CheckBox/CheckBoxContentView.swift
+++ DiffForm/CheckBox/CheckBoxContentView.swift
@@ -17,9 +17,13 @@ struct CheckBoxContentView: View {
     @State private var tasks = [Task(name: "Computer bye", isCompleted: false),Task(name: "set sound", isCompleted: false)]
     var body: some View {
         VStack{
-            List(tasks) { task in
+            List($tasks) { $task in
                 HStack{
                     Image(systemName: "square")
+                        .onTapGesture {
+                            task.isCompleted.toggle()
+                        }
+                    Text(task.name)
                 }
             }
         }
