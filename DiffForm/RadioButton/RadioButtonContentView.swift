9d64d86a968798d86594588a29f94070e97c4157
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-15 11:14:29 +0530

    create radiobuttoncontentview under radio uttton group

---
+++ DiffForm/RadioButton/RadioButtonContentView.swift
@@ -0,0 +1,20 @@
+//
+//  RadioButtonContentView.swift
+//  DiffForm
+//
+//  Created by Bhumika Patel on 15/12/22.
+//
+
+import SwiftUI
+
+struct RadioButtonContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioButtonContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButtonContentView()
+    }
+}
